0043ea3580e6455c4f95ce644fa0b76858084347
ogataka55 <ogawatakahiro2107@yahoo.co.jp>
2022-01-29 16:19:11 +0900

    seventh commit
    処理を別ファイルにまとめた

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-import requests
-
-
-def search_address(zipcode):
-    url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-    response = requests.get(url)
-    results = response.json()['results']
-    print(response.json())
-    pref_name = results[0]['address1']
-    city_name = results[0]['address2']
-    town_name = results[0]['address3']
-    address = f"{pref_name}{city_name}{town_name}"
-    return address
+from search_address import search_address
 
 
 def main():
-    zipcode = "0287111"
-    # zipcode = input("郵便番号<ハイフン無し7桁は>？")
+    # zipcode = "0287111"
+    zipcode = input("郵便番号<ハイフン無し7桁は>？")
 
     address = search_address(zipcode=zipcode)
     print(address)
