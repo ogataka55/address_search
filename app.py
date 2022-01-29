cc70419559c5b0ecd95b7c47ca5241684feb44cb
ogataka55 <ogawatakahiro2107@yahoo.co.jp>
2022-01-29 15:58:51 +0900

    sixth commit
    関数化

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import requests
 
 
-def main():
-    zipcode = "0287111"
-    # zipcode = input("郵便番号<ハイフン無し7桁は>？")
-
+def search_address(zipcode):
     url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-
     response = requests.get(url)
     results = response.json()['results']
-
     print(response.json())
     pref_name = results[0]['address1']
     city_name = results[0]['address2']
     town_name = results[0]['address3']
-
     address = f"{pref_name}{city_name}{town_name}"
+    return address
 
+
+def main():
+    zipcode = "0287111"
+    # zipcode = input("郵便番号<ハイフン無し7桁は>？")
+
+    address = search_address(zipcode=zipcode)
     print(address)
 
 
