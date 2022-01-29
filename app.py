dc3e7ff0ffa1428689c15e60462aa103619f2945
ogataka55 <ogawatakahiro2107@yahoo.co.jp>
2022-01-29 15:42:53 +0900

    fifth commit
    main関数にまとめる

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import requests
 
-zipcode = "0287111"
-# zipcode = input("郵便番号<ハイフン無し7桁は>？")
 
-url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
+def main():
+    zipcode = "0287111"
+    # zipcode = input("郵便番号<ハイフン無し7桁は>？")
 
-response = requests.get(url)
-results = response.json()['results']
+    url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
 
-print(response.json())
-pref_name = results[0]['address1']
-city_name = results[0]['address2']
-town_name = results[0]['address3']
+    response = requests.get(url)
+    results = response.json()['results']
 
-address = f"{pref_name}{city_name}{town_name}"
-print(address)
+    print(response.json())
+    pref_name = results[0]['address1']
+    city_name = results[0]['address2']
+    town_name = results[0]['address3']
+
+    address = f"{pref_name}{city_name}{town_name}"
+
+    print(address)
+
+
+if __name__ == '__main__':
+    main()
